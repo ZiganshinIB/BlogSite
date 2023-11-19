5e2f880ce6c1f4ac702d20747597673c1b3410b8
ZiganshinIB <elzig3012@gmail.com>
2023-11-19 10:44:27 +0300

    Refract model add slug

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset.filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
 
@@ -15,7 +17,8 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish') # slug будет настроен как /year/month/day/slug_name
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="blog_post")
     body = models.TextField()
@@ -31,3 +34,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # Возврат URL
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
