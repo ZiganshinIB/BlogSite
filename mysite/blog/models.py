ce2c7786579be67c4c74d436bedc3f84b9b600d1
ZiganshinIB <elzig3012@gmail.com>
2023-11-19 15:15:54 +0300

    -

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset.filter(status=Post.Status.PUBLISHED)
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
