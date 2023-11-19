94d4c0c3f6ebbd5f0bbf4690920f0323620c34a2
ZiganshinIB <elzig3012@gmail.com>
2023-11-19 17:03:06 +0300

    add email settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'djangofortest777@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
