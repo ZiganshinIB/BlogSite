9e8c27f0fa0a45b83d052df5f4c5c8b4dc596568
ZiganshinIB <elzig3012@gmail.com>
2023-11-20 11:01:26 +0300

    Refract

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,9 @@
 from .models import Post
 from .forms import EmailPostForm
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404  # , redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
-from django.views.decorators.http import require_POST
+# from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
 # Create your views here.
